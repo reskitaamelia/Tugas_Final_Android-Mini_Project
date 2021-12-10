847f63458064abeb7a9a9ccfb898eccbdad1c199
ahmadfma <83059921+reskitaamelia@users.noreply.github.com>
2021-12-11 00:03:33 +0800

    menampilkan detail resep

--- app/src/main/java/com/reskita/tugas_final_android/UI/Detail/DetailFragment.kt
+++ app/src/main/java/com/reskita/tugas_final_android/UI/Detail/DetailFragment.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.reskita.tugas_final_android.Model.Result
 import com.reskita.tugas_final_android.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.coroutines.launch
 
 class DetailFragment : Fragment() {
 
+    private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        viewModel = ViewModelProvider(this)[DetailViewModel::class.java]
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Picasso.get()
+            .load(selectedReceipt!!.thumb)
+            .into(detailThumb)
+
+        detailTitle.text = selectedReceipt!!.title
+        detailLevel.text = "Level : " + selectedReceipt!!.dificulty
+
+        lifecycleScope.launch {
+            viewModel.getDetail(selectedReceipt!!.key, view)
+        }
+
     }
 
     companion object {
--- app/src/main/java/com/reskita/tugas_final_android/UI/Detail/DetailViewModel.kt
+++ app/src/main/java/com/reskita/tugas_final_android/UI/Detail/DetailViewModel.kt
@@ -1,28 +1,53 @@
 package com.reskita.tugas_final_android.UI.Detail
 
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModel
 import com.reskita.tugas_final_android.Model.DataResep
 import com.reskita.tugas_final_android.Model.DetailResep
 import com.reskita.tugas_final_android.Model.Result
 import com.reskita.tugas_final_android.Network.RetrofitClient
+import kotlinx.android.synthetic.main.fragment_detail.view.*
 import retrofit2.Response
 import java.lang.Exception
 
 class DetailViewModel: ViewModel() {
     private val TAG = "DetailViewModel"
 
-    suspend fun getDetail(key: String) {
+    suspend fun getDetail(key: String, view: View) {
         var url = "api/recipe/$key"
 
         var response: Response<DetailResep>? = null
         try {
             response = RetrofitClient.instance.getDetail(url)
             if(response.isSuccessful) {
-
+                with(view) {
+                    val data = response!!.body()!!.results
+                    detailDesc.text = data.desc
+                    detailBahan.text = getBahan(data.ingredient)
+                    detailStep.text = getStep(data.step)
+                }
             }
         } catch (e: Exception) {
             Log.e(TAG, "${e.message}")
         }
+    }
+
+    private fun getStep(step: List<String>): String {
+        var txt = ""
+        for(stp in step) {
+            txt += "$stp\n"
+        }
+        return txt
+    }
+
+    private fun getBahan(ingredient: List<String>): String {
+        var txt = ""
+        var i = 1
+        for(bahan in ingredient) {
+            txt += "$i. $bahan\n"
+            i++
+        }
+        return txt
     }
 }
